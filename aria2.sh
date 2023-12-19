c94ec0d459b1c170548c50fd985a059573b631b0
adryd <me@adryd.com>
2023-12-18 21:39:37 -0500

    remove torsocks

--- aria2.sh
+++ aria2.sh
@@ -2,4 +2,4 @@
 # clear out index files
 # find . -type f -delete
 # disable async-dns cause torsocks nont like it
-torsocks aria2c -i ./urls.txt -j1 -s1 --async-dns=false
+aria2c -i ./urls.txt -j1 -s1 --async-dns=false
